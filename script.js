2d4dd2244a7e0807f1555d5c530d03b2e8ec3c21
GitMohammad <mohammadshamimreza23393@gmail.com>
2022-08-04 22:09:27 +0600

    complite

---
+++ script.js
@@ -0,0 +1,13 @@
+const hamburger = document.querySelector(".hamburger");
+const navManu = document.querySelector(".nav-manu");
+
+hamburger.addEventListener("click", () => {
+    hamburger.classList.toggle("active");
+    navManu.classList.toggle("active");
+})
+
+document.querySelectorAll(".nav-link").forEach(n => n.
+    addEventListener("click", () => {
+        hamburger.classList.remove("active");
+        navManu.classList.remove("active");
+    }))
